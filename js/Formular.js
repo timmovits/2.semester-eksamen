7059c7d465cac0eee6696fa41e076f714a626d97
timmovits <timm0547@edu.ucl.dk>
2020-12-08 01:24:14 +0100

    Kontakt formular ny

--- js/Formular.js
+++ js/Formular.js
@@ -1,9 +1,39 @@
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function validate(){
+  var name = document.getElementById("navn").value;
+  var subject = document.getElementById("emne").value;
+  var phone = document.getElementById("tlf nummer").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("besked").value;
+  var error_message = document.getElementById("fejl-besked");
+  
+  error_message.style.padding = "10px";
+  
+  var text;
+  if(name.length < 3){
+    text = "Indtast et navn på minimum 3 tegn";
+    error_message.innerHTML = text;
+    return false;
   }
-}// JavaScript Document
+  if(subject.length < 10){
+    text = "Minimum 10 tegns uddybelse af emnet";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(isNaN(phone) || phone.length != 8){
+    text = "Venligst indtast et fuldt telefonnummer";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Indtast en mail vi kan vende tilbage til";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(message.length <= 30){
+    text = "Beskriv emnet med minimum 30 tegn";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+  return true;
+}
