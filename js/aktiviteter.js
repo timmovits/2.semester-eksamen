1814e61b97d87e509fc227bf00949e775820e30d
SabineHurton <70168193+SabineHurton@users.noreply.github.com>
2020-12-11 11:25:16 +0100

    Aktivitets bokse (mangler indhold)

--- js/aktiviteter.js
+++ js/aktiviteter.js
@@ -6,3 +6,138 @@ function FoldUd() {
       x.style.display = "block";
     }
   }
+
+  function FoldUd2() {
+    var x = document.getElementById("EmneIndhold2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd3() {
+    var x = document.getElementById("EmneIndhold3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd4() {
+    var x = document.getElementById("EmneIndhold4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd5() {
+    var x = document.getElementById("EmneIndhold5");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd6() {
+    var x = document.getElementById("EmneIndhold6");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd7() {
+    var x = document.getElementById("EmneIndhold7");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd8() {
+    var x = document.getElementById("EmneIndhold8");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd9() {
+    var x = document.getElementById("EmneIndhold9");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd10() {
+    var x = document.getElementById("EmneIndhold10");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd11() {
+    var x = document.getElementById("EmneIndhold11");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd12() {
+    var x = document.getElementById("EmneIndhold12");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd13() {
+    var x = document.getElementById("EmneIndhold13");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd14() {
+    var x = document.getElementById("EmneIndhold14");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd15() {
+    var x = document.getElementById("EmneIndhold15");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function FoldUd16() {
+    var x = document.getElementById("EmneIndhold16");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
